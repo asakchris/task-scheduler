507666ca0f03fa36c9bab00e2282f7c31ccc7acd
Kamaraj Christopher <k.appadurai@spglobal.com>
2020-02-20 18:33:55 -0500

    Changed Date to ZonedDateTime

--- src/main/java/com/example/task/ScheduledTasks.java
+++ src/main/java/com/example/task/ScheduledTasks.java
@@ -1,25 +1,23 @@
 package com.example.task;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 @Component
 public class ScheduledTasks {
     private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
-
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss VV");
 
     @Value("${app.task.name}")
     private String name;
 
     @Scheduled(fixedRate = 5000)
     public void reportCurrentTime() {
-        log.info("Task: {}, The time is now {}", name, dateFormat.format(new Date()));
+        log.info("Task: {}, The time is now {}", name, FORMATTER.format(ZonedDateTime.now()));
     }
 }
